65fc755435b0007a8635a7392ea6d04b2c5caf0f
KusKusKusKus <rafacyda@gmail.com>
2021-05-11 18:19:13 +0300

    init

--- pinocchio/settings.py
+++ pinocchio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -163,3 +164,5 @@ PWA_APP_DIR = 'ltr'
 PWA_APP_LANG = 'ru-RU'
 
 PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR, 'static/js', 'serviceworker.js')
+
+django_heroku.settings(locals())
